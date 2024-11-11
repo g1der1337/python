180378366c3efe3b88c8f75deb0277ff7f3db91d
g1der1337 <stepanvangg228@gmail.com>
2024-11-11 20:58:51 +0300

    рммор

--- lesson.py
+++ lesson.py
@@ -1,43 +1,22 @@
-class Test():
-    def __init__(self, name):
-        self.n1 = 1000
-        self.n2 = 999
-        self.n3 = 990
-    def run(self):
-        print([self.n1, self.n2, self.n3])
-
-# ------------------------------------------------------------------------------------------
-
-
-class Cars():
-    def __init__(self, producer, model, year, speed):
-        self.producer = producer
+class Transport():
+    def __init__(self, model, year, speed):
         self.model = model
         self.year = year
         self.speed = speed
-    def info_car(self):
-        print('Это машина', self.producer, self.model, self.year, 'года выпуска, имеющая среднюю скорость', self.speed, 'км/час')
-    def speed_car(self, dist):
-        print(dist/self.speed)
+    def print_info(self):
+        print(self.model, self.year, self.speed)
+    def fun_1(self, distance):
+        self.distance =  distance
+        print(self.distance * self.speed)
+
+class Cars(Transport):
+    def __init__(self, model, year, speed, places):
+        super().__init__(model, year, speed)
+        self.places = places
 
 
-cars_p911 = Cars('Porche', '911 992 Carera 4s cabrio', 2024, 100)
-
-# ------------------------------------------------------------------------------------------
-
-class Calculator():
-    def __init__(self, num1, num2):
-        self.num1 = num1
-        self.num2 = num2
-    def addition(self):
-        print(self.num1 + self.num2)
-
-    def subtraction(self):
-        print(self.num1 - self.num2)
-
-    def multiplication(self):
-        print(self.num1 * self.num2)
-
-    def division(self):
-        print(self.num1 / self.num2)
-
+class Helicopter(Transport):
+    def __init__(self, model, year, speed):
+        super().__init__(model, year, speed)
+    def rise(self):
+        print('брбрбрбрбррбрб')
