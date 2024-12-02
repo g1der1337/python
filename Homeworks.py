07bfd9d2c9d1a9fbfd9252c6cdccb6275579b5ba
g1der1337 <stepanvangg228@gmail.com>
2024-12-02 20:08:18 +0300

    gcchgchc

--- Homeworks.py
+++ Homeworks.py
@@ -11,6 +11,11 @@
 #     case(5):
 #         print("super 5")
 
+
+
+
+
+
 # num1 = int(input())
 # num2 = int(input())
 # num3 = int(input())
@@ -23,42 +28,46 @@
 #         print(num1 / num2)
 #     case('*'):
 #         print(num1 * num2)
-# 
 
-# import json
 
-# users = {}
 
-# def create_user():
-#     username = input()
-#     password = input()
 
-#     if username in users:
-#         print('your name had added')
-#     else:
-#         users[username] = password
-#         print(f"User {username} had added.")
 
-# def delete_user():
-#     username = input()
-#     if username in users:
-#         del users[username]
 
-# def display_users():
-#     for user in users:
-#         print("-", user)
+import json
 
-# while 0 == 0:
-#     command = input()
+users = {}
 
-#     if command == "new":
-#         create_user()
-#     elif command == "del":
-#         delete_user()
-#     elif command.lower() == "exit":
-#         break
-#     else:
-#         display_users()
+def create_user():
+    username = input()
+    password = input()
 
-# with open('users.txt', 'w') as file:
-#     json.dump(users, file)
+    if username in users:
+        print('your name had added')
+    else:
+        users[username] = password
+        print(f"User {username} had added.")
+
+def delete_user():
+    username = input()
+    if username in users:
+        del users[username]
+
+def display_users():
+    for user in users:
+        print("-", user)
+
+while 0 == 0:
+    command = input()
+
+    if command == "new":
+        create_user()
+    elif command == "del":
+        delete_user()
+    elif command.lower() == "exit":
+        break
+    else:
+        display_users()
+
+with open('users.txt', 'w') as file:
+    json.dump(users, file)
