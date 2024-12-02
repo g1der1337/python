7c813c8ddb32863bc14dab6b4255c6c54bc4b665
g1der1337 <stepanvangg228@gmail.com>
2024-12-02 19:48:46 +0300

    udigzhf

--- Homeworks.py
+++ Homeworks.py
@@ -1,25 +1,64 @@
-# #1
-open_file = open("i_file.txt" , "w")
-print(open_file)
-open_file.write(input())
 
-# #2
-n = 'Тут надо писать'
-open_file_1 = open("i_file.txt" , 'a')
-open_file_1.write(n)
+# match (int(input())):
+#     case(1):
+#         print("super 1")
+#     case(2):
+#         print("super 2")
+#     case(3):
+#         print("super 3")
+#     case(4):
+#         print("super 4")
+#     case(5):
+#         print("super 5")
 
-#3
-n = 0
-while n != 'ex':
-    n = input()
-    if n == 'new':
-        name = input()
-        password = input()
-        if (name not in open_file_2) and (password not in open_file_2): 
-            open_file_2 = open("file_1.txt", 'a')
-            open_file_2.write(name, password)
-    elif n == 'del':
-        name = input()
-        password = input()
-        open_file_2.name = ''
-        open_file_2.password = ''
+# num1 = int(input())
+# num2 = int(input())
+# num3 = int(input())
+# match (num2):
+#     case('+'):
+#         print(num1 + num2)
+#     case('-'):
+#         print(num1 - num2)
+#     case(':'):
+#         print(num1 / num2)
+#     case('*'):
+#         print(num1 * num2)
+# 
+
+# import json
+
+# users = {}
+
+# def create_user():
+#     username = input()
+#     password = input()
+
+#     if username in users:
+#         print('your name had added')
+#     else:
+#         users[username] = password
+#         print(f"User {username} had added.")
+
+# def delete_user():
+#     username = input()
+#     if username in users:
+#         del users[username]
+
+# def display_users():
+#     for user in users:
+#         print("-", user)
+
+# while 0 == 0:
+#     command = input()
+
+#     if command == "new":
+#         create_user()
+#     elif command == "del":
+#         delete_user()
+#     elif command.lower() == "exit":
+#         break
+#     else:
+#         display_users()
+
+# with open('users.txt', 'w') as file:
+#     json.dump(users, file)
